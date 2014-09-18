d9e63933f68cbdf5030123d925d5ff46a7f2c625
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-09-18 15:20:25 +0200

    Allow more filters after |translate

--- services/TranslateService.php
+++ services/TranslateService.php
@@ -17,9 +17,9 @@ class TranslateService extends BaseApplicationComponent
         // Expressions for ""|t()
         'html' => array(
             // Single quotes
-            '/\{\{(| )\'(.*?)\'.*?\|.*?(t|translate)(\(.*?\)|)(| )\}\}/',
+            '/\{\{(| )\'(.*?)\'.*?\|.*?(t|translate)(\(.*?\)|).*?\}\}/',
             // Double quotes
-            '/\{\{(| )"(.*?)".*?\|.*?(t|translate)(\(.*?\)|)(| )\}\}/'
+            '/\{\{(| )"(.*?)".*?\|.*?(t|translate)(\(.*?\)|).*?\}\}/'
         ),
         
         // Expressions for Craft.t()
