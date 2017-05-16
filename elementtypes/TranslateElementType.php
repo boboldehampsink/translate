0abbcf51e8160accc6ab3faeaca7815c9caa6de1
Jan Henckens <jan@statik.be>
2017-05-16 10:56:27 +0200

    Typo

--- elementtypes/TranslateElementType.php
+++ elementtypes/TranslateElementType.php
@@ -243,7 +243,7 @@ class TranslateElementType extends BaseElementType
     {
         // If the site only has 1 locale enabled, set the translated locale to the primary (and only) locale
         if(empty($criteria['locale'])) {
-            $criteria['locale'] = craft()->i18n->getPrimarySiteLocale()
+            $criteria['locale'] = craft()->i18n->getPrimarySiteLocale();
         }
 
         $variables = array(
